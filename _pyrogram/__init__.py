f4c0531b8047c09bc9013ca73f9f562129792dc5
Kaze <101078139+KazeDevID@users.noreply.github.com>
2022-06-28 16:08:26 +0800

    KazeDevID

---
+++ _pyrogram/__init__.py
@@ -0,0 +1,28 @@
+# Pembuat (Michael Agam/Kaze) baru saja mem-Porting ini untuk Devloper Userbot # (C) 2022 KazeDevID
+
+import logging
+import sys
+import time
+import pyromod.listen
+from pyrogram import Client, errors
+from config import API_HASH, API_ID, PY_SESSION
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+LOGGER = logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+HELP = {}
+CMD_HELP = {}
+
+StartTime = time.time()
+
+API_ID = API_ID
+API_HASH = API_HASH
+PY_SESSION = PY_SESSION
+
+app = Client(PY_SESSION, api_id=API_ID, api_hash=API_HASH)
---
+++ _pyrogram/__main__.py
@@ -0,0 +1,9 @@
+# (C) 2022 KazeDevID
+
+from pyrogram import idle, Client, filters
+from _pyrogram import app, LOGGER
+from _pyrogram.modules import *
+import os
+
+print("Pyrogram User Client is Starting... \nPowered By @KenalSayaaa\n(C) 2022 KazeDevID" end="\n\n")
+app.run()
